52baf735da8b5649708f7ac34257451c21f0ca05
Jason Reich <jason.reich@anaplan.com>
2024-01-25 10:41:33 +0000

    Now with token exchange

--- auth/app.js
+++ auth/app.js
@@ -1,8 +1,11 @@
 const server = Bun.serve({
   port: 3000,
   fetch(request) {
-    if (request.headers.get('authorization') == "Bearer jason") {
-      return new Response("Absolutely!", {headers: {"X-Auth-Attributes": "he's awesome"}});
+    if (request.headers.get('Authorization') == "Bearer jason") {
+      return new Response("Absolutely!", {headers: {
+        "Authorization": "Bearer shortlivedjwt",
+        "X-Auth-Attributes": "he's awesome"
+      }});
     } else {
       return new Response("Go away!", {status: 401});
     }
